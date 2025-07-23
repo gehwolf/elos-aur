ffcc44f7f492f97d9ef37011b76de70613a9f8f6
Wolfgang Gehrhardt <gehwolf@freenet.de>
2025-07-24 01:37:55 +0200

    feat(update-all): fetch latest version to build from gtihub

--- update-all.sh
+++ update-all.sh
@@ -2,29 +2,35 @@
 CMD_DIR=$(realpath $(dirname $0))
 BASE_DIR=${CMD_DIR%%/ci}
 
-if [ $# -ne 3 ]; then
-    echo "Usage:"
-    echo "$0 <safu-version> <samconf-verison> <elos-version>"
-fi
+function get_latest_version {
+    curl -s "https://api.github.com/repos/Elektrobit/${1}/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'|cut -d "-" -f 2
+}
 
-$BASE_DIR/updateAurRepos.sh
+LATEST_SAFU_TAG=$(get_latest_version safu)
+LATEST_SAMCONF_TAG=$(get_latest_version samconf)
+LATEST_ELOS_TAG=$(get_latest_version elos)
+echo "safu ${LATEST_SAFU_TAG}"
+echo "samconf ${LATEST_SAMCONF_TAG}"
+echo "elos ${LATEST_ELOS_TAG}"
 
-CHROOT="$BASE_DIR/chroot"
-sudo rm -rf "$CHROOT"
-mkdir -p "$CHROOT"
-mkarchroot $CHROOT/root base-devel
+${BASE_DIR}/updateAurRepos.sh
 
-$BASE_DIR/updatePkgVer.sh $BASE_DIR/safu $1
+CHROOT="${BASE_DIR}/chroot"
+sudo rm -rf "${CHROOT}"
+mkdir -p "${CHROOT}"
+mkarchroot "${CHROOT}/root" base-devel
+
+"${BASE_DIR}/updatePkgVer.sh" "${BASE_DIR}/safu" "${LATEST_SAFU_TAG}"
 pushd safu
-makechrootpkg -c -r $CHROOT
+makechrootpkg -c -r "${CHROOT}"
 popd
 
-$BASE_DIR/updatePkgVer.sh $BASE_DIR/samconf $2
+"${BASE_DIR}/updatePkgVer.sh" "${BASE_DIR}/samconf" "${LATEST_SAMCONF_TAG}"
 pushd samconf
-makechrootpkg -c -r $CHROOT -I ../safu/safu-$1-1-x86_64.pkg.tar.zst
+makechrootpkg -c -r "${CHROOT}" -I "../safu/safu-${LATEST_SAFU_TAG}-1-x86_64.pkg.tar.zst"
 popd
 
-$BASE_DIR/updatePkgVer.sh $BASE_DIR/elos $3
+"${BASE_DIR}/updatePkgVer.sh" "${BASE_DIR}/elos" "${LATEST_ELOS_TAG}"
 pushd elos
-makechrootpkg -c -r $CHROOT -I ../safu/safu-$1-1-x86_64.pkg.tar.zst -I ../samconf/samconf-$2-1-x86_64.pkg.tar.zst
+makechrootpkg -c -r "${CHROOT}" -I "../safu/safu-${LATEST_SAFU_TAG}-1-x86_64.pkg.tar.zst" -I "../samconf/samconf-${LATEST_SAMCONF_TAG}-1-x86_64.pkg.tar.zst"
 popd
